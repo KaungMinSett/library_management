7d54e5018566829cb31fdcc2d8345dfda3af4238
Kaung Min Sat <kaungminsat@Kaungs-MacBook-Pro.local>
2024-02-21 14:20:28 +0630

    modified admin models

--- library/admin.py
+++ library/admin.py
@@ -3,8 +3,43 @@ from django.contrib import admin
 # Register your models here.
 from .models import Author, Book, Customer, Genre, Record
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_date', 'isbn', 'count', 'get_genres')
+    search_fields = ('title', 'author__name')
+    list_filter = ('author', 'publication_date', 'genre')
+   
+    ordering = ('-publication_date',)
+    
+    def get_genres(self, obj):
+        return ', '.join([genre.name for genre in obj.genre.all()])
+    
+    get_genres.short_description = 'Genres'
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'address')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('book', 'customer', 'issue_date', 'return_date', 'count', 'returned')
+    search_fields = ('book__title', 'customer__name')
+    list_filter = ('book', 'customer', 'returned')
+    date_hierarchy = 'issue_date'
+    ordering = ('-issue_date','customer__name')
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_book')
+    search_fields = ('name',)
+    
+    def get_book(self, obj):
+        return ', '.join([book.title for book in obj.book_set.all()])
+    
+    get_book.short_description = 'Books'
+
+
 admin.site.register(Author)
-admin.site.register(Book)   
-admin.site.register(Customer)
+admin.site.register(Book, BookAdmin)   
+admin.site.register(Customer, CustomerAdmin)
 admin.site.register(Genre)
-admin.site.register(Record)
+admin.site.register(Record, RecordAdmin)
--- library/models.py
+++ library/models.py
@@ -9,6 +9,7 @@ class Author(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     
+    
 
     def __str__(self):
         return self.name
