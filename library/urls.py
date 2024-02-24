2115ab91d75e7c4ab7b4ca7011fe31f9f9e0eb80
htoohlaoo <htoohlaoo7@gmail.com>
2024-02-24 23:50:26 +0630

    detail page added / genre error ---to fix

--- library/urls.py
+++ library/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 app_name = 'library'
 urlpatterns = [
-    path("book_detail/<int:pg_no>/",views.book_detail,name='book_detail')
+    path("<int:pg_no>/",views.book_list,name='book_list'),
+    path("detail/<int:pk>/",views.book_detail,name='book_detail')
 ]
--- library/views.py
+++ library/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
+from django.template import loader
+from .models import Book
 
 # Create your views here.
 from django.shortcuts import render
 from .models import Book
+from django.http import HttpResponse
+from django.core.paginator import Paginator
 
 def book_detail(request, pk):
     book = Book.objects.get(id=pk)
-    return render(request, 'library/book_detail.html', {'book': book})
 
-def book_list(request):
+    return render(request, 'book_detail.html', {'book': book,"colors":['amber','sky','cyan','yellow','green'],
+})
+
+def book_list(request,pg_no):
+    template = loader.get_template('books.html')
     books = Book.objects.all()
-    return render(request, 'library/book_list.html', {'books': books})
+    pageObj = Paginator(books,15)
+    if(pg_no):
+        books = pageObj.page(pg_no)
+    else:
+        books = pageObj.page(1)
+
+    return HttpResponse(template.render({'books':books},request))
